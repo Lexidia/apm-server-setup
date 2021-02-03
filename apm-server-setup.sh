04318b8ef2b08e479f65faf535bb47b1330a1a43
JulienGonzalez72 <38219527+JulienGonzalez72@users.noreply.github.com>
2021-02-03 15:21:59 +0100

    Create apm-server-setup.sh

---
+++ apm-server-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+cat <<EOF >apm-server.yml
+apm-server:
+    host: "0.0.0.0:8080"
+    secret_token: "${ES_ADDON_APM_AUTH_TOKEN}"
+    rum:
+      enabled: true
+
+output.elasticsearch:
+    hosts: ["${ES_ADDON_HOST}:443"]
+    protocol: "https"
+    username: "${ES_ADDON_APM_USER}"
+    password: "${ES_ADDON_APM_PASSWORD}"
+
+path.home: "${APP_HOME}"
+
+logging:
+    to_syslog: true
+    to_files: false
+
+EOF
